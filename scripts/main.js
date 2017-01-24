dd60db99a7ad4f2f8fe5b801952d0a6848256d67
Navin prasad <navin.pd@gmail.com>
2017-01-24 10:06:17 +0800

    java script tutorials basics

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Piece of code what does more than one action
+// Do not repeat yourself
+
+var myList = ['apples', 'ckche' , 'click'];
+
+myList.forEach(function(value, index) {
+	alert('I have ' + value + 'in my shopping bag.');
+});
