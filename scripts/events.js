df4882ef0900fa5739df03382e859646eb9fa4b3
Navin prasad <navin.pd@gmail.com>
2017-01-24 13:58:50 +0800

    basics of Jquery

--- scripts/events.js
+++ scripts/events.js
@@ -21,11 +21,11 @@ bruce.addEventListener("click", picLink);
 ben.addEventListener("click", picLink);
 
 function picLink() {
-  // var allImages = document.querySelectorAll("img");
+  var allImages = document.querySelectorAll("img");
 
-  // for (var i = 0; i < allImages.length; i++) {
-  //   allImages[i].className = "hide";
-  // }
+  for (var i = 0; i < allImages.length; i++) {
+    allImages[i].className = "hide";
+  }
 
   var picId = this.attributes["data-img"].value;
   var pic = document.getElementById(picId);
